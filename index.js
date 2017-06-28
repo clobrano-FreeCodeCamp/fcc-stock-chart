997cd17d6cecdc2e543b885511dc4700a27c17a2
Carlo Lobrano <c.lobrano@gmail.com>
2017-06-28 17:53:52 +0200

    Playing with json and data parsing

---
+++ index.js
@@ -0,0 +1,21 @@
+const https = require('https');
+
+const KEY = process.env.ALPHA_KEY
+const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=' + KEY;
+
+https.get(url, (res) => {
+  var chunk = '';
+
+  res.on('data', (data) => {
+    chunk += data;
+  });
+
+  res.on ('end', (unknown) => {
+    var json = JSON.parse(chunk);
+    console.log (json['Meta Data']['2. Symbol']);
+
+  });
+
+}).on('error', (e) => {
+  console.error(e);
+});
