fb9aabf346c3b2696cada6233dc53ea97e4df2cc
Carlo Lobrano <c.lobrano@gmail.com>
2017-06-30 23:03:54 +0200

    canvas: added first chart with real data from fixed stock
    - fixed canvas script
    - fixed https get of json data for a predefinded stock

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require ('express'),
   cookie_parser = require ('cookie-parser'),
   flash = require ('connect-flash'),
   pug = require ('pug');
+const https = require ('https');
 
 const app = express ();
 app.use (logger('dev'));
@@ -19,14 +20,49 @@ app.use (session ({
 }));
 app.use (flash ());
 
-app.set ('/views', path.join (__dirname, 'views'));
 app.set ('view engine', 'pug');
+app.set ('/views', path.join (__dirname, 'views'));
 app.use ("/bootstrap", express.static(path.join(__dirname, "/static/bootstrap")));
 app.use ("/stylesheets", express.static(path.join(__dirname, "/static/stylesheets")));
 app.use ("/js", express.static(path.join(__dirname, "/static/js")));
 
+
+const KEY = process.env.ALPHA_KEY
+const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=' + KEY;
+
 app.get ('/', (req, rsp) => {
-  rsp.render ('home', {});
+  https.get(url, (res) => {
+    var chunk = '';
+
+    res.on('data', (data) => {
+      chunk += data;
+    });
+
+    res.on ('end', (unknown) => {
+      const json = JSON.parse(chunk);
+      console.log(json);
+      const series = json['Time Series (1min)'];
+      var label = '';
+      var xvalues = [];
+      var yvalues = [];
+
+      label = json['Meta Data']['2. Symbol'];
+
+      var i = 0;
+      for (var k in series) {
+        xvalues.push (++i);
+        yvalues.push (series[k]['1. open']);
+      }
+
+      rsp.render ('home', {
+        'label': label,
+        'xvalues': xvalues,
+        'yvalues': yvalues
+      });
+    });
+  }).on('error', (e) => {
+    console.error(e);
+  });  
 });
 
 port = process.env.PORT || 3000
