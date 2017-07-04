de5590fac241ab967124090ae0167f0ac36fccee
Carlo Lobrano <c.lobrano@gmail.com>
2017-07-04 10:17:11 +0200

    wip: adding caching logic

--- app.js
+++ app.js
@@ -7,20 +7,11 @@ const express = require ('express'),
   flash = require ('connect-flash'),
   pug = require ('pug');
 const https = require ('https');
+const node_cache = require ('node-cache');
 
 const app = express ();
 var server = require ('http').createServer (app);
 var io = require ('socket.io')(server);
-
-
-io.on ('connect', client => {
-  client.emit ('connected', 'Connected with the server');
-
-  client.on ('updated-data', data => {
-    client.broadcast.emit ('update-all', 'Client notified new data');
-  });
-});
-
 
 app.use (logger('dev'));
 app.use (cookie_parser ());
@@ -50,75 +41,117 @@ var sel_series = 0;
 
 const colors = ['#3e95cd', '#7a910d', 'red', 'blue', 'green', 'yellow', 'magenta', 'cyan', 'orange' ];
 var stocks = ['GOOGL'];
+var cache = new node_cache ( {stdTTL: 100, checkperiod: 60} );
+
+io.on ('connect', client => {
+  client.emit ('connected', 'Connected with the server');
+
+  client.on ('updated-data', data => {
+    client.broadcast.emit ('update-all', 'Client notified new data');
+  });
+});
 
 app.get ('/', (req, rsp) => {
   if (stocks.length == 0) {
     return rsp.render ('home', {messages: req.flash ('error')});
   }
-  
+
   var callbacks = 0;
   var datasets = [];
   var title = null;
 
   for (i in stocks) {
     var stock = stocks [i];
-    var url = service_host + 'query?function=' + time_series_data [sel_series] + '&symbol=' + stock + '&interval=' + time_sampling [sel_sampling] + ' &apikey=' + KEY;
-    console.log ('Getting url: ' + url);
+    var query = 'query?function=' + time_series_data [sel_series] + '&symbol=' + stock + '&interval=' + time_sampling [sel_sampling];
+    var url = service_host + query + ' &apikey=' + KEY;
+    console.log ('gettin cache for ' + query);
+    var cached = cache.get (query);
+    console.log ('got cache ' + cached);
 
-    callbacks++;
+    if (cached) {
+      console.log ('Found cached values');
+      datasets.push (cached.dataset);
 
-    https.get(url, (res) => {
-      var chunk = '';
+      if (title === null)
+        title = cached.title;
 
-      res.on('data', (data) => {
-        chunk += data;
-      });
+      if (xvalues === undefined)
+        var xvalues = cached.xvalues;
 
-      res.on ('end', (unknown) => {
-        console.log ('End of data for stock');
-        const json = JSON.parse(chunk);
-        const err = json['Error Message'];
-        var xvalues = [];
-        var yvalues = [];
+      if (i >= stocks.length - 1) {
+        console.log ('Rendering the page');
+        rsp.render ('home', {
+          stocks: stocks,
+          title: title,
+          xvalues: xvalues,
+          datasets: JSON.stringify (datasets),
+          messages: req.flash ('error'),
+          updates: req.flash ('updates')
+        });
+      }
 
-        if (err) {
-          console.error (err);
-          req.flash ('error', 'Error: could not retrieve data');
-        } else{
-          title = title || json['Meta Data']['1. Information']
-          const stock_name = json['Meta Data']['2. Symbol'].toUpperCase();
-          const series = json[ time_series_name [sel_series] ];
+    } else {
+      console.log ('Getting url: ' + url);
+      callbacks++;
 
-          for (var k in series) {
-            xvalues.push ("'" + k + "'");
-            yvalues.push (series[k]['4. close']);
+      https.get(url, (res) => {
+        var chunk = '';
+
+        res.on('data', (data) => {
+          chunk += data;
+        });
+
+        res.on ('end', (unknown) => {
+          console.log ('End of data for stock');
+          const json = JSON.parse(chunk);
+          const err = json['Error Message'];
+          var yvalues = [];
+          var xvalues = [];
+
+          if (err) {
+            console.error (err);
+            req.flash ('error', 'Error: could not retrieve data');
+          } else{
+            title = title || json['Meta Data']['1. Information']
+            const stock_name = json['Meta Data']['2. Symbol'].toUpperCase();
+            const series = json[ time_series_name [sel_series] ];
+
+            for (var k in series) {
+              xvalues.push ("'" + k + "'");
+              yvalues.push (series[k]['4. close']);
+            }
+
+            var dataset = {
+              data: yvalues,
+              label: stock_name,
+              borderColor: colors [stocks.indexOf (stock_name)],
+              fill: false
+            };
+
+            cache.set (query, {
+              dataset: dataset,
+              title: title,
+              xvalues: xvalues
+            });
+            datasets.push (dataset);
           }
 
-          var dataset = {
-            data: yvalues,
-            label: stock_name,
-            borderColor: colors [callbacks - 1 ],
-            fill: false
-          };
+          callbacks--;
 
-          datasets.push (dataset);
-        }
-
-        callbacks--;
-
-        if (callbacks <= 0) {
-          console.log ('Rendering the page');
-          rsp.render ('home', {
-            stocks: stocks,
-            title: title,
-            xvalues: xvalues,
-            datasets: JSON.stringify (datasets),
-            messages: req.flash ('error'),
-            updates: req.flash ('updates')
-          });
-        }
+          if (callbacks <= 0) {
+            console.log ('Rendering the page');
+            rsp.render ('home', {
+              stocks: stocks,
+              title: title,
+              xvalues: xvalues,
+              datasets: JSON.stringify (datasets),
+              messages: req.flash ('error'),
+              updates: req.flash ('updates')
+            });
+          }
+        });
       });
-    });
+    }
   }
 });
 
@@ -149,24 +182,28 @@ app.get ('/remove/:stock', (req, rsp) => {
   rsp.redirect ('/');
 });
 
-app.get ('/select/:type/:id', (req, rsp) => {
-  var type = req.params.type;
-  var id = req.params.id;
+  app.get ('/select/:type/:id', (req, rsp) => {
+    var type = req.params.type;
+    var id = req.params.id;
 
-  if (type === 'series') {
-    if (id < time_series_data.length)
-      sel_series = id;
-  }
+    if (type === 'series') {
+      if (id < time_series_data.length) {
+        sel_series = id;
+        req.flash ('updates', 'new stuff');
+      }
+    }
 
-  if (type === 'sampling') {
-    if (id < time_sampling.length)
-      sel_sampling = id;
-  }
+    if (type === 'sampling') {
+      if (id < time_sampling.length) {
+        sel_sampling = id;
+        req.flash ('updates', 'new stuff');
+      }
+    }
 
-  rsp.redirect ('/');
-})
+    rsp.redirect ('/');
+  })
 
-port = process.env.PORT || 3000
-server.listen(port);
+  port = process.env.PORT || 3000
+  server.listen(port);
 
-console.log('Server listening on http://localhost:' + port);
+  console.log('Server listening on http://localhost:' + port);
